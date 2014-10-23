19aa902b11523c51ac6bce4eeb29a0d7dc067f8b
Scribe Author <admin@scribe.io>
2014-10-23 01:46:55 -0500

    Remove that nib shit.

--- res/Contents/Resources/main.js
+++ res/Contents/Resources/main.js
@@ -1,4 +1,19 @@
 var win = ScribeWindow.alloc.init;
 win.makeKeyAndOrderFront(null);
+win.title = 'joes favorite window';
 
-var menu = NSMenu.alloc.init;
+var menubar = NSMenu.new.autorelease;
+var appMenuItem = NSMenuItem.new.autorelease;
+menubar.addItem(appMenuItem);
+NSApp.setMainMenu(menubar);
+
+var appMenu = NSMenu.new.autorelease;
+var appName = NSProcessInfo.processInfo.processName;
+var quitTitle = NSString.stringWithFormat("Quit %@", appName);
+var quitMenuItem = NSMenuItem.alloc['initWithTitle:action:keyEquivalent:'](
+  quitTitle,
+  'terminate:',
+  'q'
+).autorelease;
+appMenu.addItem(quitMenuItem);
+appMenuItem.setSubmenu(appMenu);
--- src/main.m
+++ src/main.m
@@ -5,16 +5,10 @@
 int main(int argc, char * argv[]) {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   NSApplication *app = [NSApplication sharedApplication];
+  [app setActivationPolicy: NSApplicationActivationPolicyRegular];
 
   AppDelegate *appDelegate =  [[AppDelegate new] autorelease];
   [app setDelegate:appDelegate];
-
-  // the MainMenu is stored in a NIB file
-  NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
-  NSString *mainNibName = [infoDictionary objectForKey:@"NSMainNibFile"];
-  NSNib *mainNib = [[NSNib alloc] initWithNibNamed: mainNibName
-                                  bundle: [NSBundle mainBundle]];
-  [mainNib instantiateNibWithOwner: app topLevelObjects: nil];
 
   [app run];
 
