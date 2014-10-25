9871f442fc9230ca13d0b40979eb231df7132b1b
Scribe Author <admin@scribe.io>
2014-10-25 14:06:29 -0500

    Commit dummy specs that pass.

--- test/ScribeWindowTests.js
+++ test/ScribeWindowTests.js
@@ -1,1 +1,13 @@
-dasd
+UnitTest("Width setter sets the width", function(){
+  // var win = Scribe.Window.create({
+  //   center: true,
+  //   width: 800,
+  //   height: 900,
+  //   chrome: false
+  // });
+  AssertNotEqual(900, 800);
+});
+
+UnitTest("Width getter returns the width", function(){
+  AssertEqual(900, 900);
+});
