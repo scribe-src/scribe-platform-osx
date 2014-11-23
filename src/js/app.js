b51faba8fa53315ead3823c7349dd0ffeeb868d2
Scribe Author <admin@scribe.io>
2014-11-23 04:45:41 -0600

    Update app.js, add failing specs for Scribe.App. In business.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,31 @@
+(function(){
+
 Scribe.App.prototype._getName = function _getName() {
+  
+}
+
+Scribe.App.prototype._getIdentifier = function _getIdentifier() {
 
 }
+
+Scribe.App.prototype._getExePath = function _getExePath() {
+
+}
+
+Scribe.App.prototype._getCwd = function _getCwd() {
+
+}
+
+Scribe.App.prototype._getArguments = function _getArguments() {
+
+}
+
+Scribe.App.prototype._getEnv = function _getEnv(varName) {
+
+}
+
+Scribe.App.prototype._exit = function _exit(status) {
+  
+}
+
+})();
